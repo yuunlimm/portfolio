fb770ae65324c507c07e6452456e17aacf5cb963
Yuun <dla0267@gmail.com>
2019-07-17 00:48:53 -0700

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,14 +14,14 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    menuBranding.classList.add("show");
+    menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
     showMenu = false;
